c203fe90483724fc8a1ea5ed53c7b8920754ea15
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-01-24 01:29:46 +0100

    Fab

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# A Script that sets up your web servers for the deployment of web_static
+# Installing Nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+# Create the folders
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+<head>
+<title>
+AirBnb Clone
+</title>
+</head>
+<body>
+Welcome to AirBnb Clone
+</body>
+</html>" > '/data/web_static/releases/test/index.html'
+
+# Create a symbolic link
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# Ownership of the folder
+sudo chown -R ubuntu:ubuntu /data/
+
+# Nginx Config to serve content
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# restart Nginx
+sudo service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A Fabric Script that generates a .tgz archive from the contents of the web_static folder of your AirBnb Clone.
+"""
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """ The Function that generates .tgz archive"""
+
+    time = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local(f"tar -czvf versions/web_static_{time}.tgz web_static")
+
+        return (f"versions/web_static_{time}.tgz")
+
+    except Exception as e:
+        return None
