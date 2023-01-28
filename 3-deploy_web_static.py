3023264b4a774e56cc61e47dde8447f04b573e78
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-01-28 01:07:18 +0100

    FUll

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,24 +25,59 @@ def do_pack():
         return None
 
 
+# def do_deploy(archive_path):
+#     """distributes an archive to your web servers
+#     """
+#     if os.path.exists(archive_path) is False:
+#         return False
+#     try:
+#         arc = archive_path.split("/")
+#         base = arc[1].strip('.tgz')
+#         put(archive_path, '/tmp/')
+#         sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+#         main = "/data/web_static/releases/{}".format(base)
+#         sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+#         sudo('rm /tmp/{}'.format(arc[1]))
+#         sudo('mv {}/web_static/* {}/'.format(main, main))
+#         sudo('rm -rf /data/web_static/current')
+#         sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+#         return True
+#     except Exception:
+#         return False
+
 def do_deploy(archive_path):
-    """distributes an archive to your web servers
-    """
-    if os.path.exists(archive_path) is False:
-        return False
+    """Deploy web files to server"""
     try:
-        arc = archive_path.split("/")
-        base = arc[1].strip('.tgz')
+        if not (path.exists(archive_path)):
+            return False
+
+        # upload archive
         put(archive_path, '/tmp/')
-        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = "/data/web_static/releases/{}".format(base)
-        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-        sudo('rm /tmp/{}'.format(arc[1]))
-        sudo('mv {}/web_static/* {}/'.format(main, main))
+        # create target dir
+        arc = archive_path.split("/")
+        base = arc[-1].strip('.tgz')
+        # timestamp = archive_path[-18:-4]
+        sudo('mkdir -p /data/web_static/releases/{}/'
+             .format(base))
+
+        # uncompress archive and delete .tgz
+        pat = '/data/web_static/releases/'
+        sudo('tar -xzf /tmp/{0}.tgz -C {1}{0}/'.format(base, pat))
+
+        # remove archive
+        sudo('rm /tmp/{}.tgz'.format(base))
+
+        # move contents into host web_static
+        sudo('mv -f {1}{0}/web_static/* {1}{0}/'.format(base, pat))
+        # remove extraneous web_static dir
+        sudo('rm -rf {1}{0}/web_static'.format(base, pat))
+        # delete pre-existing sym link
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
-        return True
-    except Exception:
+
+        # re-establish symbolic link
+        sudo('sudo ln -s {1}{0} /data/web_static/current'.format(base, pat))
+    except Exception as e:
+        # print(e)
         return False
 
 
